d958892527c9c071054f14c17e0278ecb295e1eb
Ivan Gagarinov <dzencot@gmail.com>
2020-04-12 19:56:44 +0500

    refactoring

--- src/GetDiff.php
+++ src/GetDiff.php
@@ -4,55 +4,51 @@ namespace Differ\GetDiff;
 
 function getDiff(array $before, array $after): array
 {
-    $helper = function ($data1, $data2) use (&$helper) {
-        $getComparedData = function ($name, $beforeData, $afterData) use ($helper): array {
-            $isArrays = is_array($beforeData) && is_array($afterData);
-            if ($isArrays) {
-                $children = $helper($beforeData, $afterData);
+    $process = function ($first, $second) use (&$process) {
+        $allKeys = array_unique(array_merge(array_keys($first), array_keys($second)));
+        $diff = array_map(function ($key) use ($first, $second, $process) {
+            if (!array_key_exists($key, $first)) {
                 return [
-                    'name' => $name,
+                    'name' => $key,
+                    'type' => 'added',
+                    'newValue' => $second[$key],
+                ];
+            } elseif (!array_key_exists($key, $second)) {
+                return [
+                    'name' => $key,
+                    'type' => 'removed',
+                    'oldValue' => $first[$key],
+                ];
+            }
+
+            $beforeData = $first[$key];
+            $afterData = $second[$key];
+            $isArrays = is_array($beforeData) && is_array($afterData);
+
+            if ($isArrays) {
+                $children = $process($beforeData, $afterData);
+                return [
+                    'name' => $key,
                     'type' => 'nested',
                     'children' => $children,
                 ];
-            }
-
-            if ($beforeData === $afterData) {
+            } elseif ($beforeData == $afterData) {
                 return [
-                    'name' => $name,
+                    'name' => $key,
                     'type' => 'unchanged',
                     'value' => $beforeData,
                 ];
-            } elseif (!$beforeData) {
-                return [
-                    'name' => $name,
-                    'type' => 'added',
-                    'newValue' => $afterData,
-                ];
-            } elseif (!$afterData) {
-                return [
-                    'name' => $name,
-                    'type' => 'removed',
-                    'oldValue' => $beforeData,
-                ];
-            } elseif ($beforeData !== $afterData) {
-                return [
-                    'name' => $name,
-                    'type' => 'updated',
-                    'newValue' => $afterData,
-                    'oldValue' => $beforeData,
-                ];
             }
-        };
-
-        $allKeys = array_unique(array_merge(array_keys($data1), array_keys($data2)));
-        $diff = array_map(function ($key) use ($getComparedData, $data1, $data2) {
-            $beforeValue = $data1[$key] ?? null;
-            $afterValue = $data2[$key] ?? null;
-            $comparedData = $getComparedData($key, $beforeValue, $afterValue);
-            return $comparedData;
+            return [
+                'name' => $key,
+                'type' => 'updated',
+                'newValue' => $afterData,
+                'oldValue' => $beforeData,
+            ];
         }, $allKeys);
+
         return $diff;
     };
 
-    return $helper($before, $after, 1);
+    return $process($before, $after, 1);
 }
