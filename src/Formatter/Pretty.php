c07f844dfe061cd8573318ce9769e4af01c22953
Ivan Gagarinov <dzencot@gmail.com>
2020-04-15 22:15:42 +0500

    guard expression

--- src/Formatter/Pretty.php
+++ src/Formatter/Pretty.php
@@ -4,20 +4,22 @@ namespace Differ\Formatter\Pretty;
 
 function getPrettiedValue($value, int $depth): string
 {
-    if (is_array($value) || is_object($value)) {
-        $openTab = str_repeat(' ', ($depth + 1) * 4);
-        $closeTab = str_repeat(' ', $depth * 4);
-        $valueArr = array_map(function ($key) use ($value) {
-            $prettiedData = getPrettiedValue($value[$key], 0);
-            return "{$key}: {$prettiedData}";
-        }, array_keys($value));
-        $value = implode("\n", $valueArr);
-        return "{\n{$openTab}{$value}\n{$closeTab}}";
-    }
     if (is_bool($value)) {
         return $value ? 'true' : 'false';
     }
-    return $value;
+
+    if (!is_array($value) && !is_object($value)) {
+        return $value;
+    }
+
+    $openTab = str_repeat(' ', ($depth + 1) * 4);
+    $closeTab = str_repeat(' ', $depth * 4);
+    $valueArr = array_map(function ($key) use ($value) {
+        $prettiedData = getPrettiedValue($value[$key], 0);
+        return "{$key}: {$prettiedData}";
+    }, array_keys($value));
+    $value = implode("\n", $valueArr);
+    return "{\n{$openTab}{$value}\n{$closeTab}}";
 }
 
 function iter(array $tree, int $depth = 1): string
